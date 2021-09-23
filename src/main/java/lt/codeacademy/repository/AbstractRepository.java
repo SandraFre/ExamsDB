9d9d4fd6a301c0342aedfa98be12ffb9f3e98934
SandraFre <sandra.email@gmail.com>
2021-09-23 16:53:02 +0300

    AbstractRepository created

---
+++ src/main/java/lt/codeacademy/repository/AbstractRepository.java
@@ -0,0 +1,38 @@
+package lt.codeacademy.repository;
+
+import lt.codeacademy.provider.SessionFactoryProvider;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class AbstractRepository {
+    protected void changeEntity(Consumer<Session> consumer) {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = SessionFactoryProvider.getInstance().getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+        } catch (Exception e) {
+            System.out.println(e);
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    protected <T> T getEntityInformation(Function<Session, T> function){
+        try (Session session = SessionFactoryProvider.getInstance().getSessionFactory().openSession()) {
+            return function.apply(session);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+}
