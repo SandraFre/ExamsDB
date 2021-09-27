1b1c6fa8fb9b1d366fb492ad95ab5c07a30d4a93
SandraFre <sandra.email@gmail.com>
2021-09-27 13:24:50 +0300

    StudentRepository updated

--- src/main/java/lt/codeacademy/repository/StudentRepository.java
+++ src/main/java/lt/codeacademy/repository/StudentRepository.java
@@ -1,6 +1,43 @@
 package lt.codeacademy.repository;
 
+import lt.codeacademy.entity.Student;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class StudentRepository extends AbstractRepository{
 
+    public void createNewStudent(Student student){
+        changeEntity(session -> session.save(student));
+    }
+
+    public List<Student> getStudents(){
+        return getEntityInformation(session -> session.createQuery("FROM Student", Student.class).list());
+    }
+
+    public Student getStudent (Long id){
+        return getEntityInformation(session -> session.get(Student.class, id));
+    }
+
+    public void updateStudent (Student student){
+        changeEntity(session -> session.update(student));
+    }
+
+    public void updateStudentInfoById (Long id, String name, String surname){
+        changeEntity(session -> {
+            Query query = session.createQuery("UPDATE Student SET name=:name, surname=:surname WHERE id=:id");
+            query.setParameter("name", name);
+            query.setParameter("surname", surname);
+            query.setParameter("id", id);
+
+            query.executeUpdate();
+        });
+    }
+
+    public void deleteStudentById(Long id){
+        Student student = new Student();
+        student.setId(id);
+        changeEntity(session -> session.delete(student));
+    }
 
 }
